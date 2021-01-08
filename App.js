87465f3065a4bd94f0b11d21fbb93e00e0dd1567
tahirhameed99 <tahir.hameed99@gmail.com>
2021-01-08 22:28:42 +0500

    first commit

--- App.js
+++ App.js
@@ -1,106 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import HomeViewController from "./HomeViewController.js";
+import TafseerController from "./TafseerController.js";
+import BookmarkScreen from "./BookmarkScreen.js";
+import SettingScreen from "./SettingScreen.js";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-import admob, { MaxAdContentRating, InterstitialAd, RewardedAd, BannerAd, TestIds, BannerAdSize } from '@react-native-firebase/admob';
-
-class App extends Component{
-
-  componentDidMount(){
-    admob()
-    .setRequestConfiguration({
-      // Update all future requests suitable for parental guidance
-      maxAdContentRating: MaxAdContentRating.PG,
-
-      // Indicates that you want your content treated as child-directed for purposes of COPPA.
-      tagForChildDirectedTreatment: true,
-
-      // Indicates that you want the ad request to be handled in a
-      // manner suitable for users under the age of consent.
-      tagForUnderAgeOfConsent: true,
-    })
-    .then(() => {
-      // Request config successfully set!
-    });
-  }
-
-  render(){
+function App() {
   return (
-    <>
-    <View>
-
-       <BannerAd size={BannerAdSize.SMART_BANNER}
-                 unitId='ca-app-pub-9152919921144751/9500579178'>
-       </BannerAd>
-
-       <Text> In the name of Allah ... !!! </Text>
-
-     </View>
-
-     </>
+    <NavigationContainer>
+      <Stack.Navigator  screenOptions={{ headerShown: false }} initialRouteName="HomeViewController">
+        <Stack.Screen name="HomeViewController" component={HomeViewController} />
+        <Stack.Screen name="TafseerController" component={TafseerController} />
+        <Stack.Screen name="BookmarkScreen" component={BookmarkScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
